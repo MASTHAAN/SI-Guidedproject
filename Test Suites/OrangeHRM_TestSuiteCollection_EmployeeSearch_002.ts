b3fc8733ca6546fce212953991024df40101cc1b
Ik143143 <150470648+Ik143143@users.noreply.github.com>
2024-02-13 23:05:21 +0530

    Add files via upload

---
+++ Test Suites/OrangeHRM_TestSuiteCollection_EmployeeSearch_002.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>OrangeHRM_TestSuiteCollection_EmployeeSearch_002</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/OrangeHRM_TestSuite/OrangeHRM_TestSuite_EmployeeSearch_002</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
