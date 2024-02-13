1c74d8639164e29d97beac62edb7aa471f6d1779
Ik143143 <150470648+Ik143143@users.noreply.github.com>
2024-02-13 22:55:30 +0530

    Add files via upload

---
+++ Test Suites/OrangeHRM_TestSuite_Login_001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OrangeHRM_TestSuite_Login_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dc8156de-442a-4eda-9f45-f2feb2ffafa8</testSuiteGuid>
+   <testCaseLink>
+      <guid>1eefd7ee-c6ed-4bda-862a-69a43a089193</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_OrangeHRM_EmployeeSearch_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
