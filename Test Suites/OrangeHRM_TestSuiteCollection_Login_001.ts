d5cb6f8ba33cbf8567afac7ba5c0351318d2032d
Ik143143 <150470648+Ik143143@users.noreply.github.com>
2024-02-13 23:06:12 +0530

    Add files via upload

---
+++ Test Suites/OrangeHRM_TestSuiteCollection_Login_001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>OrangeHRM_TestSuiteCollection_Login_001</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>PARALLEL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>false</requireConfigurationData>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/OrangeHRM_TestSuite/OrangeHRM_TestSuite_Login_001</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
